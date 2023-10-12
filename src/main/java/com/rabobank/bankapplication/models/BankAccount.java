41793cc6b91252ba8a3d97d42d8107fcc621ab13
paulinahsainz <Paulina.Hernandez.Sainz@rabobank.nl>
2023-10-12 18:23:45 +0200

    post LOMBOK annotations

--- src/main/java/com/rabobank/bankapplication/models/BankAccount.java
+++ src/main/java/com/rabobank/bankapplication/models/BankAccount.java
@@ -1,24 +1,24 @@
 package com.rabobank.bankapplication.models;
 
 import jakarta.persistence.*;
-import com.rabobank.bankapplication.utils.IBANUtil;
-import lombok.*;
+import lombok.Data;
 
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-//POJO (plain ol' java object)
-//LOMBOK -- annotate to get instant getters & setters
-//JAVA RECORDS
 @Entity
+@Data
 public class BankAccount {
     private long balance;
+
     @Id
     public String iban;
+
     @ManyToOne(cascade = CascadeType.ALL)
     private User user;
+
     @OneToMany(mappedBy = "from")
     private List<Transaction> outgoingTransactions;
 
@@ -26,8 +26,8 @@ public class BankAccount {
     private List<Transaction> incomingTransactions;
 
     public BankAccount() {
-
     }
+
     public BankAccount(User user, String iban) {
         this.user = user;
         this.iban = iban;
@@ -36,49 +36,9 @@ public class BankAccount {
     @Transient
     public List<Transaction> getAllTransactions() {
         ArrayList<Transaction> transactionArrayList = new ArrayList<>();
-        transactionArrayList.add((Transaction) incomingTransactions);
-        transactionArrayList.add((Transaction) outgoingTransactions);
+        transactionArrayList.addAll(incomingTransactions);
+        transactionArrayList.addAll(outgoingTransactions);
         transactionArrayList.sort(Comparator.comparing(Transaction::getDate));
         return transactionArrayList;
     }
-    public long getBalance() {
-        return balance;
-    }
-
-    public void setBalance(long balance) {
-        this.balance = balance;
-    }
-
-    public String getIban() {
-        return iban;
-    }
-
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<Transaction> getOutgoingTransactions() {
-        return outgoingTransactions;
-    }
-
-    public void setOutgoingTransactions(List<Transaction> outgoingTransactions) {
-        this.outgoingTransactions = outgoingTransactions;
-    }
-
-    public List<Transaction> getIncomingTransactions() {
-        return incomingTransactions;
-    }
-
-    public void setIncomingTransactions(List<Transaction> incomingTransactions) {
-        this.incomingTransactions = incomingTransactions;
-    }
 }
-
--- src/main/java/com/rabobank/bankapplication/models/Transaction.java
+++ src/main/java/com/rabobank/bankapplication/models/Transaction.java
@@ -2,97 +2,29 @@ package com.rabobank.bankapplication.models;
 
 import com.rabobank.bankapplication.services.SortingService;
 import jakarta.persistence.*;
-
+import lombok.Data;
 import java.time.LocalDateTime;
-import java.util.Set;
-
 
 @Entity
+@Data
 public class Transaction {
-    private long amount;
-
     @Id
     @GeneratedValue
     private Long id;
+    private long amount;
     private String category;
     private LocalDateTime date;
+
     @ManyToOne
     @JoinColumn(name = "from_bank_account_id")
     private BankAccount from;
+
     @ManyToOne
     @JoinColumn(name = "to_bank_account_id")
     private BankAccount to;
+
     private String fromIban;
-
     private String toIban;
-
-    public Transaction(){};
-
-    public Transaction(long amount, LocalDateTime date, String fromIban, String category) {
-        this.amount = amount;
-        this.date = date;
-        this.fromIban = fromIban;
-        this.category = category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    public String getCategory() {
-        return category;
-    }
-
-    public long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(long amount) {
-        this.amount = amount;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public void setBankAccount(BankAccount bankAccount) {
-        this.from = bankAccount;
-    }
-    public void setTo(BankAccount to) {
-        this.to = to;
-    }
-
-    public String getFromIban() {
-        return fromIban;
-    }
-
-    public void setFromIban(String fromIban) {
-        this.fromIban = fromIban;
-    }
-
-    public void setFrom(BankAccount from) {
-        this.from = from;
-    }
-
-    public String getToIban() {
-        return toIban;
-    }
-
-    public void setToIban(String toIban) {
-        this.toIban = toIban;
-    }
-
 
     public long getCo2Emission() {
         SortingService.Category categoryEnum = SortingService.categorize(this);
@@ -126,7 +58,6 @@ public class Transaction {
 
         return co2Emission;
     }
-
 
     @Override
     public String toString() {
